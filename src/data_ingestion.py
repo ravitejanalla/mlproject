8888e3bb903165d1665712f8c4ed409d5bede809
ravitejanalla <21pa5a5407@vishnu.edu.in>
2023-03-14 23:43:11 +0530

    data transformation

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -5,7 +5,10 @@ from logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
+from dataclasses import dataclass 
+from data_transformation import DataTransformation
+from data_transformation import DataTransformationConfig
+from utils import save_object
 
 @dataclass
 class DataIngestionConfig:
@@ -46,6 +49,11 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+   
 
 
